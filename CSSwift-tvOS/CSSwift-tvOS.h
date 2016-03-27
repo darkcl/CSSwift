aaf6c5fae757a7958c892929be770bebe525a925
Yeung Yiu Hung <hkclex@gmail.com>
2016-03-27 12:03:39 +0800

    + tvOS

---
+++ CSSwift-tvOS/CSSwift-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  CSSwift-tvOS.h
+//  CSSwift-tvOS
+//
+//  Created by Yeung Yiu Hung on 27/3/2016.
+//  Copyright © 2016 Yeung Yiu Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSSwift-tvOS.
+FOUNDATION_EXPORT double CSSwift_tvOSVersionNumber;
+
+//! Project version string for CSSwift-tvOS.
+FOUNDATION_EXPORT const unsigned char CSSwift_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSwift_tvOS/PublicHeader.h>
+
+
